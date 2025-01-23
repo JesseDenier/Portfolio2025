52b083895535a651322fb82346cf948844a61786
Jesse <jessecdenier@gmail.com>
2025-01-22 23:22:35 -0600

    Moved logo to inline and made it have correct hover effects.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,3 @@
-// Imports React library
 import React from "react";
 
 const Header = () => {
@@ -7,9 +6,20 @@ const Header = () => {
       <div className="flex justify-between items-center">
         {/* Logo or Name */}
         <div className="flex items-center gap-2">
-          {/* Favicon */}
-          <img src="/logo.svg" alt="Logo" className="h-6 w-6" />
-          <h1 className="text-xl font-bold">Jesse Denier</h1>
+          {/* Inline SVG for Favicon with Hover Effect */}
+          <a
+            href="#Home"
+            className="flex items-center gap-2 hover:text-yellow transition-colors duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 142.9 173.8"
+              className="h-6 w-6 fill-current"
+            >
+              <path d="M80.6,173.8l-15.2,0L0,108.4l0-15.2h35.4c-10.2-9.9-16.6-23.7-16.6-39H34c0,18.9,13.5,34.7,31.4,38.3V0H73 c29.9,0,54.2,24.3,54.2,54.2c0,15.3-6.4,29.1-16.6,39h32.3v15.2H80.6V173.8z M21.5,108.4l44,44v-44H21.5z M80.6,15.9v76.5 C98.5,88.9,112,73.1,112,54.2S98.5,19.5,80.6,15.9z" />
+            </svg>
+            <h1 className="text-xl font-bold">Jesse Denier</h1>
+          </a>
         </div>
 
         {/* Centered Navigation Links */}
